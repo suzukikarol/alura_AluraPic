6551a93a532a9e02d2efd654d92cb02b217bfec6
Karoline Suzuki <ka.suzuki.23@gmail.com>
2021-04-12 18:41:08 -0300

    add router cache navigated

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -3,21 +3,27 @@ import { UserService } from '../user/user.service';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
 
     constructor(
         private userService: UserService,
-        private router: Router) {}
+        private router: Router) { }
 
     canActivate(
-        route: ActivatedRouteSnapshot, 
+        route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-            
-            if(!this.userService.isLogged()){
-                this.router.navigate(['']);
-                return false;
-            }
-            return true;
+
+        if (!this.userService.isLogged()) {
+            this.router.navigate(
+                [''],
+                {
+                    queryParams: {
+                        fromUrl: state.url
+                    }
+                });
+            return false;
+        }
+        return true;
     }
 }
