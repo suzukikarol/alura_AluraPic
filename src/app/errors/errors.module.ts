ccf6e33db0c247e945691abd65b8a8cccb0e8c95
Karoline Suzuki <ka.suzuki.23@gmail.com>
2021-04-12 14:32:06 -0300

    create handle error custom and validate haeder menu

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NotFoundComponent]
+  declarations: [NotFoundComponent],
+  providers: [
+    {
+      provide: ErrorHandler,//estou fazendo um de-para dizendo que no lugar do Error Handle padrão do Angular
+      useClass: GlobalErrorHandler//vou usar o Error Handle criado por mim
+    }
+  ]
 })
 export class ErrorsModule { }
--- src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -6,16 +6,30 @@ import { UserService } from "../../../core/user/user.service";
 @Directive({
     selector: '[showIfLogged]'
 })
-export class ShowIfLoggedDirective implements OnInit { 
-    
+export class ShowIfLoggedDirective implements OnInit {
+
+    currentDisplay: string;
+
     constructor(
         private element: ElementRef<any>,
         private renderer: Renderer,
         private userService: UserService
-    ) {}
+    ) { }
 
     ngOnInit(): void {
-        !this.userService.isLogged() 
-            && this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+        //diretiva carregada, peguei o valor do display atual
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+        //aguardo uma imessão do meu observable, quando ele emitir
+        //ai eu seto um loggind
+        this.userService.getUser().subscribe(user => {
+            if (user) {
+                //se tiver o loggin true mostro o display do menu
+                this.renderer.setElementStyle(this.element.nativeElement, 'diplay', this.currentDisplay)
+            } else {
+                //caso o contrário não exibo valor nenhum em menu
+                this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+            }
+        })
     }
 }
